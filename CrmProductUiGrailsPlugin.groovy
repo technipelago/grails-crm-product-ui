5b7281f748c18747581b6aa49c9b5be6ddf42be2
Goran Ehrsson <goran@technipelago.se>
2013-09-14 00:17:33 +0200

    Version 1.2.0 Grails 2.2.4

--- CrmProductUiGrailsPlugin.groovy
+++ CrmProductUiGrailsPlugin.groovy
@@ -1,28 +1,20 @@
 class CrmProductUiGrailsPlugin {
-    // Dependency group
     def groupId = "grails.crm"
-    // the plugin version
-    def version = "1.1.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0 > *"
-    // the other plugins this plugin depends on
+    def version = "1.2.0"
+    def grailsVersion = "2.2 > *"
     def dependsOn = [:]
-    def loadAfter = ['crmContact']
-    // resources that are excluded from plugin packaging
+    def loadAfter = ['crmProduct']
     def pluginExcludes = [
             "grails-app/conf/ApplicationResources.groovy",
             "src/groovy/grails/plugins/crm/product/TestSecurityDelegate.groovy",
             "grails-app/views/error.gsp"
     ]
-
     def title = "Grails CRM Product Plugin"
     def author = "Goran Ehrsson"
     def authorEmail = "goran@technipelago.se"
     def description = '''\
 Provides (admin) user interface for product/item management in Grails CRM
 '''
-
-    // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/crm-product-ui"
     def license = "APACHE"
     def organization = [name: "Technipelago AB", url: "http://www.technipelago.se/"]
@@ -35,25 +27,11 @@ Provides (admin) user interface for product/item management in Grails CRM
             link controller: "crmProduct", action: "index"
             permissions {
                 guest "crmProduct:index,list,show"
+                partner "crmProduct:index,list,show"
                 user "crmProduct:*"
                 admin "crmProduct,crmProductGroup,crmPriceList:*"
             }
             hidden true
         }
     }
-/*
-    def doWithApplicationContext = { applicationContext ->
-        def crmCoreService = applicationContext.crmCoreService
-        def crmPluginService = applicationContext.crmPluginService
-        def crmContentService = applicationContext.containsBean('crmContentService') ? applicationContext.crmContentService : null
-        if (crmContentService) {
-            crmPluginService.registerView('crmProduct', 'show', 'tabs',
-                    [id: "resources", index: 250, permission: "crmProduct:show", label: "Media", template: '/crmProduct/resources', plugin: "crm-product", model: {
-                        def result = crmContentService.findResourcesByReference(crmProduct, [sort: 'title', order: 'asc'])
-                        return [list: result, totalCount: result.totalCount, reference: crmCoreService.getReferenceIdentifier(crmProduct)]
-                    }]
-            )
-        }
-    }
-*/
 }
